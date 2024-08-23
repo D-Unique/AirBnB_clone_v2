6e20075692e066f7e5d2e788e4bdf5137eeebc95
Unigwe <emmunigwe@gmail.com>
2024-08-23 19:32:30 +0100

    done

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,14 @@
-from flask import Flask
+#!/usr/bin/python3
+"""This module creates a basic flask app and causes it to listen to 0.0.0.0"""
 
+from flask import Flask
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
 
+@app.route('/', strict_slashes=False)
 def greeting():
     return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
